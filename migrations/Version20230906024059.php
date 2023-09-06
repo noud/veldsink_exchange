b29c0d55976bd53946b1a0dabf67071303c787d4
noud <noud4@home.nl>
2023-09-06 12:40:11 +0200

    insert dispatcher

--- migrations/Version20230906024059.php
+++ migrations/Version20230906024059.php
@@ -21,6 +21,7 @@ final class Version20230906024059 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE dispatcher (id INT AUTO_INCREMENT NOT NULL, api VARCHAR(4) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('INSERT INTO dispatcher (id, api) VALUES (1, "rest")');
     }
 
     public function down(Schema $schema): void
